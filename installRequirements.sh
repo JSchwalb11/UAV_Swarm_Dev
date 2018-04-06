e65033dfac5499a100f3d2f8beda917a4d5e7aa1
jschwalb11 <joey.schwalb@gmail.com>
2018-04-06 16:33:05 -0500

    Dependencies commitment

---
+++ installRequirements.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sudo apt-get install python-pip
+sudo pip install flask
+sudo pip install flask_api
+sudo pip install requests
+sudo pip install dronekit
+sudo pip install dronekit_sitl
+sudo apt-get update
